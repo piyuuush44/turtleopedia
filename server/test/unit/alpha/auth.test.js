7ebfe7375b0af973b2383b549b66b606674f7421
jayantmawai05 <jayantmawai05@gmail.com>
2020-06-02 22:05:42 +0530

    jayant

---
+++ server/test/unit/alpha/auth.test.js
@@ -0,0 +1,120 @@
+const request = require('supertest');
+const bcrypt = require('bcrypt');
+const util = require('../../../src/utils/auth_utils');
+const app = require('../../../app');
+
+const Posts = require('../../../src/app/alpha/models/posts');
+const User = require('../../../src/app/alpha/models/user');
+
+let server;
+
+beforeEach(() => {
+  server = request(app);
+});
+
+
+describe('login api test cases', () => {
+  it('should login', async () => {
+    const user = new User();
+    user.first_name = 'turtle';
+    user.last_name = 'singh';
+    user.email = 'tech@turtleopedia.com';
+    user.password_hash = await util.getPasswordHash('password');
+
+    const finalUser = await user.save();
+   // console.log(finalUser);
+    const body = {
+      email: 'tech@turtleopedia.com',
+      password: 'password',
+    };
+    const url = '/alpha/login';
+    const response = await server.post(url).send(body);
+  //  console.log(response.body);
+    expect(response.status).toEqual(200);
+    expect(response.body.result.token.length).toBeGreaterThan(0);
+    expect(response.body.result.user._id).toEqual(finalUser._id.toString());
+    expect(response.body.result.user.email).toEqual(body.email);
+    expect(response.body.result.user.password_hash).toEqual(finalUser.password_hash);
+  });
+  it('email incorrect', async () => {
+    const user = new User();
+    user.first_name = 'turtle';
+    user.last_name = 'singh';
+    user.email = 'tech@turtleopedia.com';
+    user.password_hash = await util.getPasswordHash('password');
+
+    const finalUser = await user.save();
+  //  console.log(finalUser);
+    const body = {
+      email: 'tech@turtleopeia.com',
+      password: 'password',
+    };
+    const url = '/alpha/login';
+    const response = await server.post(url).send(body);
+    //console.log(response.body);
+    //console.log(response.status);
+    expect(response.status).toEqual(200);
+    expect(response.body.error.httpStatus).toEqual(400);
+    expect(response.body.error.message).toEqual('Invalid request! User not found');
+  });
+  it('password incorrect', async () => {
+    const user = new User();
+    user.first_name = 'turtle';
+    user.last_name = 'singh';
+    user.email = 'tech@turtleopedia.com';
+    user.password_hash = await util.getPasswordHash('password');
+
+    const finalUser = await user.save();
+   // console.log(finalUser);
+    const body = {
+      email: 'tech@turtleopedia.com',
+      password: 'passwo',
+    };
+    const url = '/alpha/login';
+    const response = await server.post(url).send(body);
+   // console.log(response.body);
+    //console.log(response.status);
+    expect(response.status).toEqual(200);
+    expect(response.body.error.httpStatus).toEqual(400);
+    expect(response.body.error.message).toEqual('Invalid request! Wrong Password');
+  });
+});
+
+describe('registration api test cases',()=>{
+it('Registration Successful',async ()=>{
+  const user = new User();
+  user.first_name = 'turtle';
+  user.last_name = 'singh';
+  user.email = 'tech@turtleopedia.com';
+  user.password_hash = await util.getPasswordHash('password');
+  const finalUser = await user.save();
+  const body = {
+    email: 'tech@turtleopdia.com',
+    password: 'password',
+  };
+  const url = '/alpha/register';
+  const response = await server.post(url).send(body);
+  //console.log(response.status);
+  //console.log(response.body);
+  expect(response.body.result.user.email).toEqual(body.email);
+});
+it('User Already Exist',async ()=>{
+  const user = new User();
+  user.first_name = 'turtle';
+  user.last_name = 'singh';
+  user.email = 'tech@turtleopedia.com';
+  user.password_hash = await util.getPasswordHash('password');
+  const finalUser = await user.save();
+  const body = {
+    email: 'tech@turtleopedia.com',
+    password: 'password',
+  };
+  const url = '/alpha/register';
+  const response = await server.post(url).send(body);
+ // console.log(response.status);
+ // console.log(response.body);
+  expect(response.status).toEqual(200);
+  expect(response.body.error.httpStatus).toEqual(400);
+  expect(response.body.error.message).toEqual('User already found ');
+});
+});
