ab72bfe34bb448724e6e13c79fec94ffdd52eee0
skynet apps <skynetaps@gmail.com>
2020-06-06 22:56:54 +0530

    Fix: package step

--- deployments/scripts/server_package.sh
+++ deployments/scripts/server_package.sh
@@ -3,9 +3,8 @@
 set -e
 
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
-#pip install docker
 
 cd $PROJECT_ROOT/server
 
-docker build -f ./bin/$CI_ENVIRONMENT_SLUG/Dockerfile --pull -t $CI_REGISTRY_IMAGE:dev .
+docker build -f ./bin/$CI_ENVIRONMENT_SLUG/Dockerfile --pull -t $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG .
 docker push $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG
