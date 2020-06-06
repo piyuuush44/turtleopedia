cbc1dc6f1d15b89c1c21f847960584fa84c5ddce
skynet apps <skynetaps@gmail.com>
2020-06-06 21:44:02 +0530

    Fix: package step

--- deployments/scripts/server_package.sh
+++ deployments/scripts/server_package.sh
@@ -7,5 +7,5 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
 cd $PROJECT_ROOT/server
 
-docker build -f ./bin/$CI_ENVIRONMENT_SLUG/Dockerfile --pull -t $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG .
+docker build -f ./bin/$CI_ENVIRONMENT_SLUG/Dockerfile --pull -t $CI_REGISTRY_IMAGE:dev .
 docker push $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG
