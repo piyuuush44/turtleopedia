323998766721dcba264e718d7f97470d572bf95e
skynet apps <skynetaps@gmail.com>
2020-06-07 00:22:03 +0530

    Add: gcp

--- deployments/scripts/server_package.sh
+++ deployments/scripts/server_package.sh
@@ -9,14 +9,14 @@ cd $PROJECT_ROOT/server
 #docker build -f ./bin/$CI_ENVIRONMENT_SLUG/Dockerfile --pull -t $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG .
 
 
-#docker build -f ./bin/$CI_ENVIRONMENT_SLUG/Dockerfile --pull -t $DOCKER_IMAGE_TAG:$CI_ENVIRONMENT_SLUG .
+docker build -f ./bin/$CI_ENVIRONMENT_SLUG/Dockerfile --pull -t $DOCKER_IMAGE_TAG:$CI_ENVIRONMENT_SLUG .
 
 
 
 #docker push $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG
 echo $GCP_SERVICE_KEY
 echo "$GCP_SERVICE_KEY" >key.json
-docker login -u _json_key --password-stdin https://eu.gcr.io <key.json
+docker login -u _json_key --password-stdin https://asia.gcr.io <key.json
 
 docker push $DOCKER_IMAGE_TAG:$CI_ENVIRONMENT_SLUG
 #$GCP_SERVICE_KEY  > ${HOME}/gcloud-service-key.json
