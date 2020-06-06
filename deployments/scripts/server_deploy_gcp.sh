81dce93bfe86b665d17cc78628263d6f9858e2ec
skynet apps <skynetaps@gmail.com>
2020-06-06 23:09:04 +0530

    Add: gcp

--- deployments/scripts/server_deploy_gcp.sh
+++ deployments/scripts/server_deploy_gcp.sh
@@ -1,1 +1,11 @@
+#!/bin/sh
 
+set -e
+
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+
+cd $PROJECT_ROOT/server
+
+docker pull $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG
+docker tag $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG gcr.io/turtleopedia/turtleopedia:$CI_ENVIRONMENT_SLUG
+docker push gcr.io/turtleopedia/turtleopedia:$CI_ENVIRONMENT_SLUG
