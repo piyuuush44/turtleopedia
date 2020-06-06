998d6630059a320184720ef8bdf3dd801ab41f7d
skynet apps <skynetaps@gmail.com>
2020-06-06 18:56:34 +0530

    Fix: package step

--- deployments/scripts/server_lint.sh
+++ deployments/scripts/server_lint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 cd $DIR
 source setup.sh
 
--- deployments/scripts/server_package.sh
+++ deployments/scripts/server_package.sh
@@ -1,4 +1,3 @@
-7
 #!/bin/sh
 
 set -e
@@ -7,8 +6,6 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
 cd $PROJECT_ROOT/server
 
-
-docker login -u gitlab-ci-token -p $CI_JOB_TOKEN registry.gitlab.com
 
 docker build -f ./bin/$CI_ENVIRONMENT_SLUG/Dockerfile --pull -t $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG .
 docker push $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG
