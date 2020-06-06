a2ed4bea0e841dfa765ff2e41e5b07ac8aa95f3b
skynet apps <skynetaps@gmail.com>
2020-06-07 00:39:40 +0530

    Add: gcp

--- deployments/scripts/server_deploy_gcp.sh
+++ deployments/scripts/server_deploy_gcp.sh
@@ -12,6 +12,6 @@ echo "$GCP_SERVICE_KEY" >key.json
 gcloud auth activate-service-account --key-file key.json
 gcloud config set project turtleopedia
 gcloud config set compute/zone us-central1-c
-gcloud container clusters get-credentials turtleopedia-production
+gcloud container clusters get-credentials turtleopedia
 kubectl set image deployment/turtleopedia-production turtleopedia-production=$DOCKER_IMAGE_TAG:$CI_ENVIRONMENT_SLUG
 
