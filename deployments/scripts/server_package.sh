5c26bf0b635174d9c373c26e9cab6932f6e5d509
skynet apps <skynetaps@gmail.com>
2020-06-06 23:28:53 +0530

    Add: gcp

--- deployments/scripts/server_package.sh
+++ deployments/scripts/server_package.sh
@@ -6,5 +6,13 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
 cd $PROJECT_ROOT/server
 
-docker build -f ./bin/$CI_ENVIRONMENT_SLUG/Dockerfile --pull -t $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG .
-docker push $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG
+#docker build -f ./bin/$CI_ENVIRONMENT_SLUG/Dockerfile --pull -t $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG .
+docker build -f ./bin/$CI_ENVIRONMENT_SLUG/Dockerfile --pull -t gcr.io/turtleopedia/turtleopedia:$CI_ENVIRONMENT_SLUG .
+#docker push $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG
+docker push gcr.io/turtleopedia/turtleopedia:$CI_ENVIRONMENT_SLUG
+
+
+#$GCP_SERVICE_KEY  > ${HOME}/gcloud-service-key.json
+#docker login -u _json_key --password-stdin https://gcr.io < ${HOME}/gcloud-service-key.json
+#docker build -t gcr.io/projectid/app:${CI_COMMIT_SHA} .
+#docker push "gcr.io/projectid/app:${CI_COMMIT_SHA}"
