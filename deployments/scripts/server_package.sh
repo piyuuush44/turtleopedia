2ed0da8b0d6eefd81429edcebe611b7f1a9a2a99
skynet apps <skynetaps@gmail.com>
2020-06-06 21:13:32 +0530

    Fix: package step

--- deployments/scripts/server_package.sh
+++ deployments/scripts/server_package.sh
@@ -3,9 +3,11 @@
 set -e
 
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
+pip install docker
 
 cd $PROJECT_ROOT/server
 
 docker info
+docker login -u gitlab-ci-token -p $CI_JOB_TOKEN registry.gitlab.com
 docker build -f ./bin/$CI_ENVIRONMENT_SLUG/Dockerfile --pull -t $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG .
 docker push $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG
