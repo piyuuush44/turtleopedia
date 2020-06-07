7b13c2fc29a5b3cf7d0ce30244b66d523f35b9bd
skynet apps <skynetaps@gmail.com>
2020-06-07 19:30:58 +0530

    Fix: deploy script set image

--- deployments/scripts/server_package.sh
+++ deployments/scripts/server_package.sh
@@ -6,20 +6,11 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
 cd $PROJECT_ROOT/server
 
-#docker build -f ./bin/$CI_ENVIRONMENT_SLUG/Dockerfile --pull -t $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG .
-
-
 docker build -f ./bin/$CI_ENVIRONMENT_SLUG/Dockerfile --pull -t $DOCKER_IMAGE_TAG:$CI_ENVIRONMENT_SLUG .
 
-
-
-#docker push $CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG
 echo $GCP_SERVICE_KEY
 echo "$GCP_SERVICE_KEY" >key.json
 docker login -u _json_key --password-stdin https://asia.gcr.io <key.json
 
 docker push $DOCKER_IMAGE_TAG:$CI_ENVIRONMENT_SLUG
-#$GCP_SERVICE_KEY  > ${HOME}/gcloud-service-key.json
-#docker login -u _json_key --password-stdin https://gcr.io < ${HOME}/gcloud-service-key.json
-#docker build -t gcr.io/projectid/app:${CI_COMMIT_SHA} .
-#docker push "gcr.io/projectid/app:${CI_COMMIT_SHA}"
+
