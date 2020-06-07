693a9d68ef4cd465df2fc5b03cfd600ba834cbd1
skynet apps <skynetaps@gmail.com>
2020-06-07 17:47:13 +0530

    Fix: package step

--- deployments/scripts/server_deploy_gcp.sh
+++ deployments/scripts/server_deploy_gcp.sh
@@ -10,8 +10,19 @@ echo $GCP_K8_KEY
 echo "$GCP_K8_KEY" >key.json
 
 gcloud auth activate-service-account --key-file key.json
-gcloud config set project turtleopedia
-gcloud config set compute/zone us-central1-c
-gcloud container clusters get-credentials turtleopedia-production --zone=us-central1-c
-kubectl set image deployment/turtleopedia-production turtleopedia-production=$DOCKER_IMAGE_TAG:$CI_ENVIRONMENT_SLUG
 
+echo "Authenticated the key"
+
+gcloud config set project turtleopedia
+
+echo "Project set successfully"
+
+gcloud config set compute/zone us-central1-c
+
+echo "Zone set successfully"
+
+gcloud container clusters get-credentials turtleopedia-production --zone=us-central1-c
+
+echo "Cluster auth set successfully"
+
+kubectl set image deployment/turtleopedia-production turtleopedia-production=$DOCKER_IMAGE_TAG:$CI_ENVIRONMENT_SLUG
