ec181f78a5502786be820e5f05120f458a1d7869
skynet apps <skynetaps@gmail.com>
2020-06-08 13:42:08 +0530

    Fix: changed error messge in delta/health

--- server/src/app/delta/controllers/health.js
+++ server/src/app/delta/controllers/health.js
@@ -1,5 +1,5 @@
 const logger = require('../../../config/logger');
 exports.get = async (req, res) => {
   logger.info(process.env);
-  return res.status(200).json('Working fine......Great!!');
+  return res.status(200).json('Everything working really good......Great!!');
 };
