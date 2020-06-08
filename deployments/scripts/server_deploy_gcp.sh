7ce0e8d585d9125285304b04e2ab25b4d5e3fdca
skynet apps <skynetaps@gmail.com>
2020-06-08 14:42:41 +0530

    Fix:deployment latest image

--- deployments/scripts/server_deploy_gcp.sh
+++ deployments/scripts/server_deploy_gcp.sh
@@ -9,22 +9,23 @@ cd $PROJECT_ROOT/server
 echo "$GCP_K8_KEY" >key.json
 
 gcloud auth activate-service-account --key-file key.json
-
 echo "Authenticated the key"
 
 gcloud config set project turtleopedia
-
 echo "Project set successfully"
 
 gcloud config set compute/zone us-central1-c
-
 echo "Zone set successfully"
 
 gcloud container clusters get-credentials turtleopedia-production --zone=asia-south1-b
-
 echo "Cluster auth set successfully"
 
-kubectl set image deployment/turtleopedia-production turtleopedia-sha256-1=$CI_REGISTRY_IMAGE:latest -n=turtleopedia-production
+kubectl apply -f file:///$PROJECT_ROOT/deployments/gke/turtleopedia-production-configmap.yml
 
+kubectl delete --all pods -n=turtleopedia-production
+echo "All previous pods deleted successfully"
+
+kubectl set image deployment/turtleopedia-production turtleopedia-sha256-1=$CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG -n=turtleopedia-production
 echo "Latest GCR Image set successfully"
-#file:///$PROJECT_ROOT/deployments/gke/turtleopedia-production.yml
+
+#
