9f3224e83cca26d6da8f1bb8feaf28381d8af8ff
Manish <mv005123@gmail.com>
2020-06-20 13:46:31 +0530

    Completed top 5 posts

--- server/src/app/alpha/controllers/dashboard.js
+++ server/src/app/alpha/controllers/dashboard.js
@@ -4,7 +4,10 @@ const ClientError = require('../../../errors').client;
 exports.getDashboardData = async (req, res, next) => {
   try {
     return res.json({
-      featuredposts: featuredPosts.getFeaturedPosts(),
+      result:{
+        featuredPosts: featuredPosts.getFeaturedPosts(),
+      },
+      message:'Posts returned Successfully!'
     });
   } catch (e) {
     return next(new ClientError({message: e.message}));
