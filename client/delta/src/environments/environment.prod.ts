911a51790926cbd255b9d19fec5bef712799feb6
skynet apps <skynetaps@gmail.com>
2020-06-22 17:19:10 +0530

    fixed angular api endpoints

--- client/delta/src/environments/environment.prod.ts
+++ client/delta/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://34.93.238.173'
 };
--- server/src/app/delta/utils/website.js
+++ server/src/app/delta/utils/website.js
@@ -10,7 +10,7 @@ exports.categoryCount = async () => Posts.aggregate(
       }],
 );
 
-exports.getRecentPosts = async () => Posts.find().limit(5)
+exports.getRecentPosts = async () => Posts.find().limit(6)
     .sort({created_at: -1});
 
 exports.getTopPosts = async () => Posts.find({is_top: true});
