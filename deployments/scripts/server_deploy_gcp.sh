153089a87f4ffd27873bf0ae9fb4b648581ebc40
skynet apps <skynetaps@gmail.com>
2020-06-23 21:51:05 +0530

    A

--- deployments/scripts/server_deploy_gcp.sh
+++ deployments/scripts/server_deploy_gcp.sh
@@ -11,22 +11,22 @@ echo "$GCP_K8_KEY" >key.json
 gcloud auth activate-service-account --key-file key.json
 echo "Authenticated the key"
 
-gcloud config set project turtleopedia
+gcloud config set project my-turtleopedia
 echo "Project set successfully"
 
-gcloud config set compute/zone us-central1-c
+gcloud config set compute/zone asia-south1-b
 echo "Zone set successfully"
 
-gcloud container clusters get-credentials turtleopedia-production --zone=asia-south1-b
+gcloud container clusters get-credentials turtleopedia --zone=asia-south1-b
 echo "Cluster auth set successfully"
 
-kubectl apply -f ../deployments/gke/turtleopedia-production-configmap.yaml
+#kubectl apply -f ../deployments/gke/turtleopedia-production-configmap.yaml
 #kubectl apply -f file:///$PROJECT_ROOT/deployments/gke/turtleopedia-production-configmap.yaml
 
-kubectl delete --all pods -n=turtleopedia-production
+kubectl delete --all pods -n=turtleopedia
 echo "All previous pods deleted successfully"
 
-kubectl set image deployment/turtleopedia-production turtleopedia-sha256-1=$CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG -n=turtleopedia-production
+kubectl set image deployment/turtleopedia turtleopedia=$CI_REGISTRY_IMAGE:$CI_ENVIRONMENT_SLUG -n=turtleopedia
 echo "Latest GCR Image set successfully"
 
 #
