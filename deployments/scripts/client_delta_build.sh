c9a5b36003e73760ac8363dd9ec4a00619999edb
skynet apps <skynetaps@gmail.com>
2020-06-24 17:08:38 +0530

    Add: delta ci cd

---
+++ deployments/scripts/client_delta_build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+
+cd $PROJECT_ROOT/client/delta
+
+# Install the dependencies
+echo "Installing dependencies..."
+npm install
+
+# Build
+echo "Building..."
+npm build --prod --aot
