e17947ba46f938ff26a31d23e1d46e4b33f6bb52
skynet apps <skynetaps@gmail.com>
2020-06-25 16:56:00 +0530

    changed messge in health

--- server/src/app/delta/controllers/health.js
+++ server/src/app/delta/controllers/health.js
@@ -1,5 +1,5 @@
 const logger = require('../../../config/logger');
 exports.get = async (req, res) => {
   logger.info(process.env);
-  return res.status(200).json('Everything working really good......Great!!');
+  return res.status(200).json('Everything working really good....!!');
 };
