e4def5bfdc500665efcac2b2dd33c1e86e58bfcf
skynet apps <skynetaps@gmail.com>
2020-06-28 22:04:32 +0530

    Fixed small masonbox error

--- client/delta/src/app/app.component.ts
+++ client/delta/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AppState} from './store/app.reducer';
 import {Store} from '@ngrx/store';
+import * as CoreActions from './core/store/core.actions';
 
 @Component({
     selector: 'app-root',
@@ -9,11 +10,12 @@ import {Store} from '@ngrx/store';
 })
 export class AppComponent implements OnInit {
     constructor(private store: Store<AppState>) {
-
     }
 
     title = 'delta';
 
     ngOnInit() {
+        this.store.dispatch(CoreActions.TRY_FETCH_WEBSITE_DATA());
+        this.store.dispatch(CoreActions.TRY_FETCH_FILTER_POSTS({payload: ''}));
     }
 }
