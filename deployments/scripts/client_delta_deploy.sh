2928b6eaccb1910f7af4eb90b478f7743e670eba
skynet apps <skynetaps@gmail.com>
2020-07-06 22:56:10 +0530

    Lint fix

--- deployments/scripts/client_delta_deploy.sh
+++ deployments/scripts/client_delta_deploy.sh
@@ -14,6 +14,8 @@ echo "$FIREBASE_DEPLOY_KEY"
 npm install -g firebase-tools
 echo "Installed firebase tools"
 
+npm install
+
 firebase use --token $FIREBASE_DEPLOY_KEY $FIREBASE_CI_PROJECT
 echo "Added project $FIREBASE_CI_PROJECT"
 
