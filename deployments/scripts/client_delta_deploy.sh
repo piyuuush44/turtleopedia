9b7456069dbfaf0a41b42f9504aba10feed8af19
skynet apps <skynetaps@gmail.com>
2020-07-06 20:19:12 +0530

    Lint :Delta

--- deployments/scripts/client_delta_deploy.sh
+++ deployments/scripts/client_delta_deploy.sh
@@ -7,5 +7,6 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 cd $PROJECT_ROOT/client/delta
 
 npm install -g firebase-tools
+npm i
 firebase use --token $FIREBASE_DEPLOY_KEY production
 firebase deploy --non-interactive --token $FIREBASE_DEPLOY_KEY
