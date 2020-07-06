2dabf1c879f45d72f94d145d87f8a21d0b6e1df2
skynet apps <skynetaps@gmail.com>
2020-07-06 21:22:10 +0530

    Lint fix

--- deployments/scripts/client_delta_deploy.sh
+++ deployments/scripts/client_delta_deploy.sh
@@ -7,6 +7,14 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 cd $PROJECT_ROOT/client/delta
 
 npm install -g firebase-tools
-firebase use --add my-turtleopedia
+echo "Installed firebase tools"
+
 firebase use --token $FIREBASE_DEPLOY_KEY
+echo "Using token $FIREBASE_DEPLOY_KEY"
+
+firebase use --token $FIREBASE_DEPLOY_KEY --add my-turtleopedia
+echo "Added project"
+
+echo "Deploying now"
 firebase deploy --non-interactive --token $FIREBASE_DEPLOY_KEY
+echo "Deployed"
