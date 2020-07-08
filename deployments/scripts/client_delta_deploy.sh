5eaaa188f22d6f2b046c836cc7042325a54129b0
skynet apps <skynetaps@gmail.com>
2020-07-09 00:10:01 +0530

    Add:Ssr

--- deployments/scripts/client_delta_deploy.sh
+++ deployments/scripts/client_delta_deploy.sh
@@ -15,7 +15,5 @@ firebase use --token $FIREBASE_DEPLOY_KEY $FIREBASE_CI_PROJECT
 echo "Added project $FIREBASE_CI_PROJECT"
 
 echo "Deploying now"
-npm i
-npm run deploy
-#firebase deploy --non-interactive --token $FIREBASE_DEPLOY_KEY
+firebase deploy --non-interactive --token $FIREBASE_DEPLOY_KEY
 echo "Deployed"
