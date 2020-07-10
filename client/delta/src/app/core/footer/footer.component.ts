878369676c69757cdbf4e521432be756f7e85370
skynet apps <skynetaps@gmail.com>
2020-07-10 11:26:04 +0530

    Fixed: footer

--- client/delta/src/app/core/footer/footer.component.ts
+++ client/delta/src/app/core/footer/footer.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WebdataModel} from "../model/webdata.model";
+import {select, Store} from "@ngrx/store";
+import {AppState} from "../../store/app.reducer";
+import {coreStateWebsiteDataSelector} from "../store/core.selector";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+    data: WebdataModel = new WebdataModel([], [], [], ['lifestyle']);
 
-  constructor() { }
+    constructor(private store: Store<AppState>) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.store.pipe(select(coreStateWebsiteDataSelector)).subscribe(
+            value => {
+                this.data = value;
+            }
+        );
+    }
 
 }
