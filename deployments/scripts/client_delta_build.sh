8af814231bc83330415473732665a2fa3c275142
skynet apps <skynetaps@gmail.com>
2020-07-14 19:05:30 +0530

    Fix: merged package to build

--- deployments/scripts/client_delta_build.sh
+++ deployments/scripts/client_delta_build.sh
@@ -10,6 +10,24 @@ cd $PROJECT_ROOT/client/delta
 echo "Installing dependencies..."
 npm install
 
+ROUTE=sitemap
+SITEMAP_DESTINATION=${PROJECT_ROOT}/client/delta/${ROUTE}
+
+# make the directory where sitemap files will be stored
+mkdir -p $SITEMAP_DESTINATION
+
+date
+print_ok "Starting generate_sitemap script"
+
+# generating sitemap package
+npm run generate_sitemap --d $SITEMAP_DESTINATION --r $ROUTE
+date
+
+print_ok 'Printing sitemap file'
+cat $SITEMAP_DESTINATION/sitemap.xml
+
+print_ok "Done with generate_sitemap script"
+
 # Build
 echo "Building..."
 npm run build:ssr
