b0778dd0ec7ee91ea1380f856cefd13de1d44b99
skynet apps <skynetaps@gmail.com>
2020-07-14 22:33:22 +0530

    f

--- client/delta/dist/index.js
+++
@@ -1,9 +0,0 @@
-const functions = require('firebase-functions');
-
-const expressApp = require('./dist/server/main').app();
-
-exports.ssr = functions
-  .region('us-central1')
-  .runWith({"timeoutSeconds":60,"memory":"1GB"})
-  .https
-  .onRequest(expressApp);
