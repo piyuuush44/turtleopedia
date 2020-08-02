a99fe6fd29995fb7e520475390d974603b884e3a
skynet apps <skynetaps@gmail.com>
2020-08-02 20:35:16 +0530

    Fix: / route

--- server/src/app/neon/routes/index.js
+++ server/src/app/neon/routes/index.js
@@ -1,14 +1,12 @@
 const health = require('./health');
-const neon = require('./neon');
 
 module.exports = () => {
   let routes = [];
 
   routes = routes.concat(health);
-  routes = routes.concat(neon);
 
   routes.forEach((r) => {
-    r.route = `/neon${r.route}`;
+    r.route = `/${r.route}`;
   });
 
   return routes;
